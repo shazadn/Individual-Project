ab4b037b287c9e9da8ef1d8423fe21d846c017d1
Shazad Nasim <s.nasim@hotmail.co.uk>
2019-06-12 23:57:06 +0100

    Update User CRUD functionality

---
+++ src/main/java/com/qa/model/User.java
@@ -0,0 +1,28 @@
+package com.qa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	int id;
+	String userName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
