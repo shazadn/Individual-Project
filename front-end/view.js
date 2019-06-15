47a49cf038716c5111bd9c99ae1fd0e6dca0c779
Shazad Nasim <s.nasim@hotmail.co.uk>
2019-06-15 19:03:40 +0100

    Added GET functionality, able view players

---
+++ front-end/view.js
@@ -0,0 +1,35 @@
+
+
+function viewPlayerCards() {
+    fetchData("playercard", 'GET', null)
+        .then((r) => {
+            console.log("Successful");
+            console.log('PlayerCard', r);
+            return JSON.parse(r);
+        }).then((data) => {
+            return generateRows(data);
+        }).catch((reason) => {
+            console.log(reason)
+        });
+}
+
+
+function generateRows(players) {
+    for (let player of players) {
+        player.actions = '<a href="update.html?id=' + player.id +'">Update</a>';
+        console.log(player);
+        createTableRow(player);
+    }
+}
+
+function createTableRow(player) {
+    let tableRef = document.getElementById("players-table");
+    let row = tableRef.insertRow(-1);
+
+    let index = 0;
+    for (let key in player) {
+        let cell = row.insertCell(index);
+        cell.innerHTML = player[key];
+        index++;
+    }
+}
