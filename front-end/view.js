36942699885cb286779907eeeffb6cb7f4e8cee6
Shazad Nasim <s.nasim@hotmail.co.uk>
2019-06-16 10:40:54 +0100

    Added delete functionality

--- front-end/view.js
+++ front-end/view.js
@@ -14,10 +14,26 @@ function viewPlayerCards() {
         });
 }
 
+function deletePlayer(id) {
+    if (confirm('Are you sure you want to delete it?')) {
+        fetchData('playercard/' + id, 'DELETE', null)
+            .then((x) => {
+                console.log(x);
+                document.getElementById("players-table").innerHTML = "";
+            
+                document.getElementById("viewPlayers").click();
+            }).catch((reason) => {
+                console.log(reason);
+            });
+    }
+
+    return false;
+}
 
 function generateRows(players) {
     for (let player of players) {
-        player.actions = '<a href="update.html?id=' + player.id + '">Update</a><br />';
+        player.actions = '<a href="update.html?id=' + player.id + '">Update</a><br />' +
+            '<a href="#" onclick="deletePlayer(' + player.id + ')">Delete</a> ';
         console.log(player);
         createTableRow(player);
     }
